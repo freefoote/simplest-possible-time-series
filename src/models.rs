2798b25f124ba1a54388c594791fe2e5fae8cbb5
Daniel Foote <freefoote@gmail.com>
2025-06-15 10:24:05 +0800

    Ok - got something to insert into the database.

---
+++ src/models.rs
@@ -0,0 +1,22 @@
+use crate::schema::tsdata;
+use chrono::DateTime;
+use chrono::Utc;
+use diesel::prelude::*;
+use serde_json;
+
+#[derive(Queryable, Selectable, Debug)]
+#[diesel(table_name = tsdata)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct TsData {
+    pub id: i32,
+    pub inserted_time: DateTime<Utc>,
+    pub series_name: String,
+    pub contents: serde_json::Value,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = tsdata)]
+pub struct NewTsData<'a> {
+    pub series_name: &'a str,
+    pub contents: &'a serde_json::Value,
+}
--- src/test-data-generator.rs
+++ src/test-data-generator.rs
@@ -1,3 +1,28 @@
+use diesel::prelude::*;
+use serde_json::json;
+
+mod helpers;
+mod models;
+mod schema;
+
 fn main() {
-    println!("Hello, world!");
+    let connection = &mut helpers::establish_connection();
+
+    let example_contents = json!({
+        "test": 10,
+        "more": 20.03
+    });
+
+    let new_entry = models::NewTsData {
+        series_name: "Test",
+        contents: &example_contents,
+    };
+
+    let returned = diesel::insert_into(schema::tsdata::table)
+        .values(&new_entry)
+        .returning(models::TsData::as_returning())
+        .get_result(connection)
+        .expect("Error saving new entry.");
+
+    print!("Returned: {:?}", returned);
 }
